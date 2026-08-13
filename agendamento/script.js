ecc0b4de04d481ba82114a451efad96fae4edbd8
adriano020 <adriano020@gmail.com>
2026-08-13 19:34:44 -0300

    teste03

--- agendamento/script.js
+++ agendamento/script.js
@@ -12,7 +12,7 @@ function reservar(linha, coluna, cadeira){
     // Verifica se a cadeira está livre
     if (sala[linha][coluna]==0){
         // Muda a Matriz para Ocupado
-        sala[linha][coluna]==1
+        sala[linha][coluna]=1
         // Muda a Cor da Cadeira para Vermelho
         cadeira.style.backgroundColor="red"
         document.getElementById("mensagem").innerText="Cadeira Reservada com Sucesso"
